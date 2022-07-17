bfa65bfa95914cb51700f99862d4fe19460e9f89
zhuzhongju <77977624+zhuzhongju@users.noreply.github.com>
2022-07-17 22:36:11 +0800

    Update code_shallow_NN.py
    Add comments on the code

--- code_shallow_NN/code_shallow_NN.py
+++ code_shallow_NN/code_shallow_NN.py
@@ -6,6 +6,26 @@ import logistic_regression_catproblem as lr
 
 
 def layer_size(X, Y):
+    """
+    Define model sturcture
+
+    Parameters
+    ----------
+    X : numpy array
+        Examples.
+    Y : numpy array
+        Ture label for examples.
+
+    Returns
+    -------
+    n_x : scalar
+        Size of the input layer.
+    n_h : scalar
+        Size of the hidden layer.
+    n_y : scalar
+        Size of the output layer.
+
+    """
     n_x = X.shape[0]
     n_h = 4
     n_y = Y.shape[0]
@@ -13,6 +33,24 @@ def layer_size(X, Y):
 
 
 def initialize_parameters(n_x, n_h, n_y):
+    """
+    Initialize weights w and bias b randomly.
+
+    Parameters
+    ----------
+    n_x : scalar
+        Size of the input layer.
+    n_h : scalar
+        Size of the hidden layer.
+    n_y : scalar
+        Size of the output layer.
+
+    Returns
+    -------
+    parameters : dictionary
+        A dictionary that contians initialized weights and bias.
+
+    """
     W1 = np.random.randn(n_h, n_x) * 0.01
     b1 = np.zeros((n_h, 1))
     W2 = np.random.randn(n_y, n_h) * 0.01
@@ -24,6 +62,22 @@ def initialize_parameters(n_x, n_h, n_y):
     return parameters
     
 def forward(parameters, X):
+    """
+    Implement forward propagation
+
+    Parameters
+    ----------
+    parameters : dictionary
+        A dictionary that contains weights and bias.
+    X : numpy array
+        Examples
+
+    Returns
+    -------
+    cache : dictionary
+        A dictionary that contains intermediate values.
+
+    """
     Z1 = np.dot(parameters["W1"], X) + parameters["b1"]
     A1 = np.tanh(Z1)
     Z2 = np.dot(parameters["W2"], A1) + parameters["b2"]
@@ -35,6 +89,22 @@ def forward(parameters, X):
     return cache
 
 def compute_cost(cache, Y):
+    """
+    Compute cost
+
+    Parameters
+    ----------
+    cache : dictionary
+        Intermediate values that were calculated from forward propagation.
+    Y : numpy array
+        True label for examples.
+
+    Returns
+    -------
+    cost : numpy array
+        negative log-likelihood cost for neural networks.
+
+    """
     A2 = cache["A2"]
     m = Y.shape[1]
     cost = -1/m * np.sum(Y * np.log(A2) + (1 - Y) * np.log(1 - A2))
@@ -42,6 +112,26 @@ def compute_cost(cache, Y):
     return cost
 
 def backward(parameters, cache, X, Y):
+    """
+    Implement backward propagation.
+
+    Parameters
+    ----------
+    parameters : dictionary
+        A dictionary that contains weights and bias.
+    cache : dictionary
+        Intermediate values that were calculated from forward propagation.
+    X : numpy array
+        Examples.
+    Y : numpy array
+        Ture label for examples.
+
+    Returns
+    -------
+    grads : dictionary
+        A dictionary that contains gradients of the loss.
+
+    """
     m = Y.shape[1]
     A2 = cache["A2"]
     dZ2 = A2 - Y
@@ -57,6 +147,24 @@ def backward(parameters, cache, X, Y):
     return grads
 
 def update_params(grads, parameters, learning_rate):
+    """
+    Update parameters after every iteraton.
+
+    Parameters
+    ----------
+    grads : dictionary
+        A dictionary that contains gradients of the loss.
+    parameters : dictionary
+        A dictionary that contains weights and bias.
+    learning_rate : scalar
+        Learning rate of the optimization algorithm.
+
+    Returns
+    -------
+    parameters : dictionary
+        A dictionary contains updated parameters(weights w and bias b).
+
+    """
     parameters["W2"] = parameters["W2"] - learning_rate * grads["dW2"]
     parameters["b2"] = parameters["b2"] - learning_rate * grads["db2"]
     parameters["W1"] = parameters["W1"] - learning_rate * grads["dW1"]
@@ -64,11 +172,50 @@ def update_params(grads, parameters, learning_rate):
     return parameters
 
 def predict(parameters, X):
+    """
+    Make prediction based on the well-trained parameters.
+
+    Parameters
+    ----------
+    parameters : dictionary
+        A dictionary that contains weights and bias.
+    X : numpy array
+        Examples.
+
+    Returns
+    -------
+    Y_predictions : numpy array
+        A numpy array (vector) containing all predictions (0/1) for the examples in X.
+
+    """
     cache = forward(parameters, X)
     Y_predictions = (cache["A2"] > 0.5)
     return Y_predictions
  
 def nn_model(X_train, Y_train, learning_rate, iterations_num):
+    """
+    Put functions together to build a 2-layers NN model
+
+    Parameters
+    ----------
+    X_train : numpy array
+        training set represented by a numpy array of shape.
+    Y_train : numpy array
+        training labels represented by a numpy array (vector) of shape.
+    learning_rate : scalar
+        hyperparameter representing the learning rate used in the update rule of optimize().
+    learning_rate : scalar
+        hyperparameter representing the learning rate used in the update rule of optimize().
+
+    Returns
+    -------
+    parameters : dictionary
+        A dictionary that contains well-trained parameters(weights w and bias b).
+    costs : list
+        A list contains negative log-likelihood cost for neural networks for every iteration.
+
+    """
+    costs = []
     n_x, n_h, n_y = layer_size(X_train, Y_train)
     parameters = initialize_parameters(n_x, n_h, n_y)
     for i in range(iterations_num):
@@ -76,22 +223,34 @@ def nn_model(X_train, Y_train, learning_rate, iterations_num):
         cost = compute_cost(cache, Y_train)
         grads = backward(parameters, cache, X_train, Y_train)
         parameters = update_params(grads, parameters, learning_rate) 
-    return parameters
+        costs.append(cost)
+    return parameters, costs
     
-   
+# load data by using pre-built function and visualize them   
 X, Y = load_planar_dataset()
 plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral);
 
-parameters = nn_model(X, Y, learning_rate = 1.2, iterations_num = 1000)
+# Train a 2-layer NN model and make a prediction based on it.
+parameters, costs = nn_model(X, Y, learning_rate = 1.2, iterations_num = 1000)
 predictions = predict(parameters, X)
+
+# Utilize logistic regression model to predict.
+train_accuracy, test_accracy, LR_params, LR_costs = lr.logistic_regression_model(X, Y, X, Y, iterations_number = 1000, learning_rate = 1.2)   
+
+# Visualize outcomes
 plt.figure(2) 
 plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y)
 
-train_accuracy, test_accracy, LR_preditions, LR_params = lr.logistic_regression_model(X, Y, X, Y, iterations_number = 1000, learning_rate = 1.2)   
-plt.figure(3) 
+plt.figure(3)
+plt.title("NN_cost")
+plt.xlabel("Number of Iterations")
+plt.ylabel("cost")
+plt.plot(np.linspace(0, len(costs), len(costs)), costs)
+
+plt.figure(4) 
 plot_decision_boundary(lambda x: lr.predict(LR_params["w"], LR_params["b"], x.T), X, Y)
 
-print ('Accuracy: %d' % float((np.dot(Y, predictions.T) + np.dot(1 - Y, 1 - predictions.T)) / float(Y.size) * 100) + '%')    
+print ('2-layer nn model_accuracy: %d' % float((np.dot(Y, predictions.T) + np.dot(1 - Y, 1 - predictions.T)) / float(Y.size) * 100) + '%')    
 print ('Logistic_regression accuracy: %d' % test_accracy + '%')
     
     
